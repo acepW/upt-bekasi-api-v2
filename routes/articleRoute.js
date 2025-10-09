d86d805c6b0c188a81cb8713e73b32b7c80a5b0a
acepW <acepwahyu001@gmail.com>
2025-10-09 10:43:31 +0700

    update

--- routes/articleRoute.js
+++ routes/articleRoute.js
@@ -2,8 +2,8 @@ const router = require("express").Router();
 const ArticleController = require("../controller/articleController");
 const { auth } = require("../middlewares/authMiddlewares");
 
-router.get("/article", auth, ArticleController.getArticle);
-router.get("/article/:id", auth, ArticleController.getArticle);
+router.get("/article", ArticleController.getArticle);
+router.get("/article/:id", ArticleController.getArticle);
 router.post("/article", auth, ArticleController.createArticle);
 router.put("/article/:id", auth, ArticleController.updateArticle);
 router.delete("/article/:id", auth, ArticleController.deleteArticle);
--- routes/videoRoute.js
+++ routes/videoRoute.js
@@ -2,8 +2,8 @@ const router = require("express").Router();
 const VideoController = require("../controller/videoController");
 const { auth } = require("../middlewares/authMiddlewares");
 
-router.get("/video", auth, VideoController.getVideo);
-router.get("/video/:id", auth, VideoController.getVideo);
+router.get("/video", VideoController.getVideo);
+router.get("/video/:id", VideoController.getVideo);
 router.post("/video", auth, VideoController.createVideo);
 router.put("/video/:id", auth, VideoController.updateVideo);
 router.delete("/video/:id", auth, VideoController.deleteVideo);
